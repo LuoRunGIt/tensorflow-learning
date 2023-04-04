98c50b69363a0317b51aa9b8f372cea3b7336ed0
LuoRunGIt <46641960+LuoRunGIt@users.noreply.github.com>
2023-04-04 22:01:14 +0800

    初始化方式

---
+++ 初始化w和b的方法/基础模型.py
@@ -0,0 +1,19 @@
+# 导入工具包
+import tensorflow as tf
+import tensorflow.keras as keras
+import tensorflow.keras.layers as layers
+#sequential方式
+# 定义model,构建模型
+model = keras.Sequential([
+    # 第一个隐层
+    layers.Dense(3, activation="relu", kernel_initializer="he_normal", name="layer1",input_shape=(3,)),
+    # 第二个隐层
+    layers.Dense(2, activation="relu",
+                 kernel_initializer="he_normal", name="layer2"),
+    # 输出层
+    layers.Dense(2, activation="sigmoid",
+                 kernel_initializer="he_normal", name="layer3")
+    ],
+    name="sequential"
+)
+model.summary()
